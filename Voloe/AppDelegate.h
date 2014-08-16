c23a4325c0021a3f50d5a22645b23823ce09f7ff
Jeremy Moyers <jmoyers14@gmail.com>
2014-08-15 21:17:47 -0700

    Initial Commit

---
+++ Voloe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Voloe
+//
+//  Created by Jeremy Moyers on 8/15/14.
+//  Copyright (c) 2014 Jeremy Moyers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Voloe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Voloe
+//
+//  Created by Jeremy Moyers on 8/15/14.
+//  Copyright (c) 2014 Jeremy Moyers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
